b81aeedf1a060f14e342a575c2ffedc5fb6a8ac3
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-28 15:11:47 +0700

    menambahkan import untuk komponen Compose, Material3, utilitas layout, ViewModel, serta resource aplikasi yang dibutuhkan untuk membangun layar input siswa dan mendukung interaksi serta penyimpanan data

--- app/src/main/java/com/example/myroomsatu/view/HalamanEntry.kt
+++ app/src/main/java/com/example/myroomsatu/view/HalamanEntry.kt
@@ -1,5 +1,38 @@
 package com.example.myroomsatu.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myroomsatu.viewmodel.DetailSiswa
+import com.example.myroomsatu.viewmodel.EntryViewModel
+import com.example.myroomsatu.viewmodel.UIStateSiswa
+import com.example.myroomsatu.viewmodel.provider.PenyediaViewModel
+import com.example.myroomsatu.view.route.DestinasiEntry
+import com.example.myroomsatu.R
+import kotlinx.coroutines.launch
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntrySiswaScreen(
