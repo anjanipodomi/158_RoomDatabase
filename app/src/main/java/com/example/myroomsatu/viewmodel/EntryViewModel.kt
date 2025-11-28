1cba20c96699ec1ab2e0ecae7331e86d0d8298e4
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-28 14:14:11 +0700

    menambahkan data class DetailSiswa sebagai model data untuk menyimpan informasi siswa seperti id, nama, alamat, dan telepon

--- app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
@@ -32,3 +32,10 @@ data class UIStateSiswa(
     val isEntryValid: Boolean = false
 )
 
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
