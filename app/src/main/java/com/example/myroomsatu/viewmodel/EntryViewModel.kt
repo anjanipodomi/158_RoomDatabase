0945b196d3e8478ec6ee41b350b2b47ad0c6efbd
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-28 14:14:49 +0700

    menambahkan fungsi ekstensi toSiswa untuk mengonversi DetailSiswa menjadi objek Siswa sebelum disimpan ke database

--- app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
@@ -39,3 +39,10 @@ data class DetailSiswa(
     val telpon: String = ""
 )
 
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
