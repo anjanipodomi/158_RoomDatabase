854916cde58629fb1b4868878a9ae264a0725045
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-28 14:57:30 +0700

    menambahkan import untuk komponen navigasi Compose, NavHostController, serta screen dan destinasi navigasi (Home dan Entry) yang diperlukan untuk membangun alur navigasi aplikasi

--- app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
@@ -1,5 +1,17 @@
 package com.example.myroomsatu.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myroomsatu.view.EntrySiswaScreen
+import com.example.myroomsatu.view.HomeScreen
+import com.example.myroomsatu.view.route.DestinasiHome
+import com.example.myroomsatu.view.route.DestinasiEntry
+
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier) {
@@ -12,23 +24,16 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
-    NavHost(
-        navController = navController,
-        startDestination = DestinasiHome.route,
-        modifier = Modifier
-    ) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier) {
         composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiEntry.route)
-                }
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
             )
         }
 
         composable(DestinasiEntry.route) {
-            EntrySiswaScreen(
-                navigateBack = { navController.popBackStack() }
-            )
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
         }
     }
 }
