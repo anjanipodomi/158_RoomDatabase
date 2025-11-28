85db87d53ada65698172aeb70f485be09dbe055f
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-28 09:46:10 +0700

    membuat Entity Siswa untuk tabel 'tblSiswa' yang berisi kolom id, nama, alamat, dan telpon

---
+++ app/src/main/java/com/example/myroomsatu/room/Siswa.kt
@@ -0,0 +1,10 @@
+package com.example.myroomsatu.room
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
