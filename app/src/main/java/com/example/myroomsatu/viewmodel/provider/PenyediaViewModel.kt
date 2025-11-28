13573ecb9291a6c87109e628930899b332d4e1ca
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-28 13:59:39 +0700

    menambahkan PenyediaViewModel dengan factory yang menginisialisasi HomeViewModel dan EntryViewModel menggunakan repositori dari aplikasi. Juga menambahkan fungsi ekstensi aplikasiSiswa() untuk mempermudah akses instance AplikasiSiswa dari CreationExtras

---
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.myroomsatu.viewmodel.provider
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
