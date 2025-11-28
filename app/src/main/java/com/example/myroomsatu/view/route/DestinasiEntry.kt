237e5f131e274ca283983c1274b56cdd5a67cada
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-28 13:46:24 +0700

    menambahkan DestinasiEntry untuk kebutuhan navigasi item entry

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.R
+import com.example.myroomsatu.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
