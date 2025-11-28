427a5138c54b196997a2ed38a1d212008c603625
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-28 14:03:08 +0700

    menambahkan import untuk komponen ViewModel, CreationExtras, serta kelas aplikasi dan ViewModel yang diperlukan agar penyediaan ViewModel dapat berjalan dengan benar dalam arsitektur aplikasi.

--- app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.myroomsatu.viewmodel.provider
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomsatu.repositori.AplikasiSiswa
+import com.example.myroomsatu.viewmodel.EntryViewModel
+import com.example.myroomsatu.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
