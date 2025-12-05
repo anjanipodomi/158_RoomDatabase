e675c620ad4171e3a95989743da32463c7e694ef
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 14:20:55 +0700

    menambahkan import R

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiDetailSiswa.kt
@@ -1,5 +1,7 @@
 package com.example.myroomsatu.view.route
 
+import com.example.myroomsatu.R
+
 object DestinasiDetailSiswa : DestinasiNavigasi {
         override val route = "detail_siswa"
         override val titleRes = R.string.detail_siswa
