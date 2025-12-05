73528206baba3ae8db98b68fc49d88962ea91b23
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 10:22:04 +0700

    menambahkan top app bar dan floating action button pada DetailSiswaScreen

--- app/src/main/java/com/example/myroomsatu/view/uicontroller/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/HalamanDetailSiswa.kt
@@ -7,4 +7,23 @@ fun DetailSiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {}
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetailSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.uiDetailState.collectAsState()
+            FloatingActionButton(
+                onClick = {
+                    //navigateToEditItem(uiState.value.detailSiswa.id)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
+            )
+        }
