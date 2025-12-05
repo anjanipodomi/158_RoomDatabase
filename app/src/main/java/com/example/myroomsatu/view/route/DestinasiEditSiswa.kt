98583f544ac9a03f5649125e1214d983012de6ec
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 13:07:09 +0700

    menambahkan DestinasiEditSiswa beserta argumen idSiswa dan route dinamis

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.myroomsatu.view.route
+
+object DestinasiEditSiswa {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
