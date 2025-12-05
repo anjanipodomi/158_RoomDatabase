6484a0e36bee8342536f67d531d356322d9aafd2
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 13:14:18 +0700

    menambahkan bagian awal dari EditViewModel yang bertugas menyiapkan data sebelum halaman edit dibuka. Pada bagian ini, ViewModel mengambil id siswa dari SavedStateHandle, lalu menggunakan repository untuk memuat data siswa tersebut. Data yang diterima kemudian diubah menjadi UIStateSiswa agar bisa langsung dipakai di tampilan. Selain itu, juga disiapkan state awal uiStateSiswa agar halaman bisa menampilkan nilai default sebelum data selesai dimuat

---
+++ app/src/main/java/com/example/myroomsatu/viewmodel/EditViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.myroomsatu.viewmodel
+
+class EditViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoriSiswa.getSiswaStream(idSiswa)
+                .filterNotNull()
+                .first()
+                .toUiStateSiswa(true)
+        }
+    }
+}
