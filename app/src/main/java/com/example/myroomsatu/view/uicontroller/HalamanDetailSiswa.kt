9e0f6456eb1b451b506e562700180bd4e94ef710
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 10:22:46 +0700

    menambahkan ikon Edit pada FloatingActionButton di DetailSiswaScreen

--- app/src/main/java/com/example/myroomsatu/view/uicontroller/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/HalamanDetailSiswa.kt
@@ -25,5 +25,11 @@ fun DetailSiswaScreen(
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
 
-            )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+
         }
