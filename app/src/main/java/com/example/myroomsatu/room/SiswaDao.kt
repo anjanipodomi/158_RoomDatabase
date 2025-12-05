ea55cbf9dc62031d0d18aa66811fe361abb5897e
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 09:12:37 +0700

    menambahkan @query dan @delete di SiswaDao

--- app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
@@ -10,7 +10,13 @@ interface SiswaDao {
     @Query(value= "SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
-
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query( "SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
 }
