4d0042faefea9a969b933250f297812abd78f55e
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 09:54:10 +0700

    menambahkan TIMEOUT_MILLIS pada companion object untuk konfigurasi SharingStarted

--- app/src/main/java/com/example/myroomsatu/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/DetailViewModel.kt
@@ -20,6 +20,11 @@ class DetailViewModel (
     suspend fun deleteSiswa(){
         repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
     }
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
 
 
 
