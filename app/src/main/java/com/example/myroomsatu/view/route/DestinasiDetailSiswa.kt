f7bb1a1639c3ac3ed18b9b23e76a25553fa75d8f
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 09:39:48 +0700

    menambahkan destinasi detail siswa dengan argumen siswaId

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,8 @@
+package com.example.myroomsatu.view.route
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+        override val route = "detail_siswa"
+        override val titleRes = R.string.detail_siswa
+        const val itemIdArg = "idSiswa"
+        val routeWithArgs = "$route/{$itemIdArg}"
+}
