e752458fe2cff753928eca1d1d13c2543973150d
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-05 09:22:58 +0700

    fungsi stream siswa + fitur delete

--- app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -5,8 +5,11 @@ import com.example.myroomsatu.room.SiswaDao
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
-
     suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+    //suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -14,4 +17,10 @@ class OfflineRepositoriSiswa(
 ):RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
+    //override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+
 }
